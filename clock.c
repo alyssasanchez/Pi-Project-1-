c94c7b73d6b5aa6e594776132d8bedfbded0b4ba
alyssasanchez <127165900+alyssasanchez@users.noreply.github.com>
2023-04-14 12:54:48 -0400

    Create clock.c

---
+++ clock.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include "FBCurses.h"
+
+void convertTime(int val1, colorcode_t oncolor, colorcode_t offcolor, int column){
+        int count = 7;
+        while(val1 != 0){
+                if( val1 %2 == 0){
+                        sexPixel(count,column,offcolor);
+                }
+                else{
+                        setPixel(count,column,oncolor);
+                }
+                val1 = val1 /2;
+                count --;
+        }
+        for (int i = count; i >= 0; i--) {
+                setPixel(i,column, offcolor);
+        }
+}
+void showTime(colorcode_t oncolor, colorcode_t offcolor){
+        time_t originaltime;
+        struct tm * timeinfo;
+        time ( &rawtime );
+        timeinfo = localtime ( &rawtime );
+
+        binaryTime(timeinfo->tm_hour, oncolor, offcolor, 1);
+        binaryTime(timeinfo->tm_min, oncolor, offcolor, 3);
+        binaryTime(timeinfo->tm_sec, oncolor, offcolor, 5);
+
+
+int main(){
+        initScreen();
+        //feel free to change the color scheme to your liking.
+        colorcode_t background=initColor(COLOR_RED);
+        colorcode_t offcolor=initColor(COLOR_BLACK);
+        colorcode_t oncolor=initColor(COLOR_WHITE);
+        clearScreen(background);
+        while (!kbhit()){
+                showTime(oncolor,offcolor);
+                sleep(1);  //sleep for 1 second
+        }
+        cleanupScreen();
+}
